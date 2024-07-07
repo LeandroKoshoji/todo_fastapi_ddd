1af9c7a9fa2e36d06375b06e2a9ee6fa2fa5222a
Leandro Koshoji <leandro.koshoji@gmail.com>
2024-07-07 18:18:04 -0300

    feat: add settings

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from app.main import app
+from app.shared.infrastructure.database.database import Base
+
+
+@pytest.fixture()
+def client():
+    return TestClient(app)
+
+
+@pytest.fixture()
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    Base.metadata.drop_all(engine)
