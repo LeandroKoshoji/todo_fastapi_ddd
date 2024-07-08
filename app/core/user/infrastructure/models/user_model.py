291dbc8e3a10bae66de915bc6eb263641e882384
Leandro Koshoji <leandro.koshoji@gmail.com>
2024-07-08 19:39:50 -0300

    feat: add uuid for pk

--- app/core/user/infrastructure/models/user_model.py
+++ app/core/user/infrastructure/models/user_model.py
@@ -1,12 +1,13 @@
 from sqlalchemy import Column, DateTime, Integer, String, func
 
 from app.core.shared.infrastructure.database.database import Base
+from app.core.shared.infrastructure.database.guid import GUID
 
 
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(GUID(), primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
