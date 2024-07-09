5f5dba5ea4c0cf5ac9682a815f58e2d59110a449
Leandro Koshoji <leandro.koshoji@gmail.com>
2024-07-09 00:01:45 -0300

    feat: add user response schema

--- app/core/user/api/schemas/user_schemas.py
+++ app/core/user/api/schemas/user_schemas.py
@@ -5,3 +5,21 @@ class CreateUserSchema(BaseModel):
     username: str
     email: EmailStr
     password: str
+
+
+class CreateUserResponseSchema(BaseModel):
+    id: str
+    username: str
+    email: str
+    created_at: str
+
+
+class LoginUserSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class LoginUserResponseSchema(BaseModel):
+    token: str
+    token_type: str
+    expires_in: str
