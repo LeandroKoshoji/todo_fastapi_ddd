4f09feeb9a2a7d6edd6f7592222590cb5f0c7aee
Leandro Koshoji <leandro.koshoji@gmail.com>
2024-07-08 23:42:22 -0300

    fix: remove warning from generic

--- app/core/shared/application/schemas/response.py
+++ app/core/shared/application/schemas/response.py
@@ -1,12 +1,11 @@
 from typing import Generic, Optional, TypeVar
 
 from pydantic import BaseModel
-from pydantic.generics import GenericModel
 
 T = TypeVar('T')
 
 
-class ResponseModel(GenericModel, Generic[T]):
+class ResponseModel(BaseModel, Generic[T]):
     status: str
     message: str
     data: Optional[T] = None
