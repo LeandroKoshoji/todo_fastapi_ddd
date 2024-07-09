fe9eb92153f87497b9aa7899dbfe8d79069feee8
Leandro Koshoji <leandro.koshoji@gmail.com>
2024-07-08 21:53:38 -0300

    fix: uuid for model

--- app/core/user/infrastructure/models/user_model.py
+++ app/core/user/infrastructure/models/user_model.py
@@ -1,15 +1,15 @@
 from sqlalchemy import Column, DateTime, String, func
 
 from app.core.shared.infrastructure.database.database import Base
-from app.core.shared.infrastructure.database.guid import GUID
+from sqlalchemy.dialects.postgresql import UUID as SQLAlchemyUUID
 
 
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(GUID(), primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
+    id = Column(SQLAlchemyUUID(as_uuid=True), primary_key=True, index=True)
+    username = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String)
     created_at = Column(
         DateTime(timezone=True), server_default=func.now(), nullable=False
