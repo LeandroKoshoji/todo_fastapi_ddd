f7a8a273e43827865d1658e7aebea40b6d936135
Leandro Koshoji <leandro.koshoji@gmail.com>
2024-07-09 22:24:24 -0300

    feat: add task repository interface

---
+++ app/core/task/domain/task_repository.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+from app.core.tasks.domain.task import Task
+
+
+class TaskRepository(ABC):
+    def save_task(self, task: Task) -> Task:
+        raise NotImplementedError
+
+    def update_task(self, task: Task) -> Task:
+        raise NotImplementedError
+
+    def get_task_by_id(self, task_id: str) -> Task | None:
+        raise NotImplementedError
+
+    def delete_task(self, task_id: str) -> None:
+        raise NotImplementedError
+
+    def get_all_tasks(self) -> list[Task]:
+        raise NotImplementedError
+
+    def get_tasks_by_user_id(self, user_id: str) -> list[Task]:
+        raise NotImplementedError
