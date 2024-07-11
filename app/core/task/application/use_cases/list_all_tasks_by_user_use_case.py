9608f3b4b2ffdba5163dca7fe39d0f0d9c2944b7
Leandro Koshoji <leandro.koshoji@gmail.com>
2024-07-10 21:56:21 -0300

    feat: add list all tasks by user use case

---
+++ app/core/task/application/use_cases/list_all_tasks_by_user_use_case.py
@@ -0,0 +1,11 @@
+from typing import List
+from app.core.task.domain.task import Task
+from app.core.task.domain.task_repository import TaskRepository
+
+
+class ListAllTasksByUserUseCase:
+    def __init__(self, task_repository: TaskRepository):
+        self.task_repository = task_repository
+
+    def execute(self, user_id: str) -> List[Task]:
+        return self.task_repository.get_tasks_by_user_id(user_id)
