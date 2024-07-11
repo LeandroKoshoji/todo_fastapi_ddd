d5c830a71f79f080e23e8b35095537a9beae8eac
Leandro Koshoji <leandro.koshoji@gmail.com>
2024-07-10 21:30:20 -0300

    feat: add edit task route

--- app/core/task/api/routes/task_routes.py
+++ app/core/task/api/routes/task_routes.py
@@ -8,7 +8,9 @@ from app.core.shared.security.dependecies import get_current_user
 from app.core.task.application.use_cases.create_task_use_case import (
     CreateTaskUseCase,
 )
+from app.core.task.application.use_cases.edit_task_use_case import EditTaskUseCase
 from app.core.task.domain.commands.create_task_command import CreateTaskCommand
+from app.core.task.domain.commands.edit_task_command import EditTaskCommand
 from app.core.task.domain.exceptions import InvalidDomainRuleError
 from app.core.task.infra.repositories.sqlalchemy_task_repository import (
     SqlAlchemyTaskRepository,
@@ -40,6 +42,29 @@ def create_task(input: CreateTaskSchema, current_user=Depends(get_current_user),
     try:
         event = use_case.execute(command)
         return success_response(event, "Task created successfully")
+    except InvalidDomainRuleError as e:
+        raise HTTPException(status_code=400, detail=str(e.message))
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.put("/{task_id}")
+def edit_task(task_id: str, input: CreateTaskSchema, current_user=Depends(get_current_user), db: Session = Depends(get_db)):
+    task_repository = SqlAlchemyTaskRepository(db)
+    use_case = EditTaskUseCase(task_repository)
+
+    command = EditTaskCommand(
+        id=task_id,
+        title=input.title,
+        user_id=current_user.get("id"),
+        status=input.status,
+        description=input.description,
+        send_notification=input.send_notification
+    )
+
+    try:
+        event = use_case.execute(command)
+        return success_response(event, "Task edited successfully")
     except InvalidDomainRuleError as e:
         raise HTTPException(status_code=400, detail=str(e.message))
     except ValueError as e:
