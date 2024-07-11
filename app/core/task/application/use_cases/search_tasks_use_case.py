2019b968edc3a0455bb63133bc9cec038b25a934
Leandro Koshoji <leandro.koshoji@gmail.com>
2024-07-10 22:33:29 -0300

    feat: add search tasks use_case

---
+++ app/core/task/application/use_cases/search_tasks_use_case.py
@@ -0,0 +1,11 @@
+from typing import List
+from app.core.task.domain.task import Task
+from app.core.task.domain.task_repository import SearchTaskFilters, TaskRepository
+
+
+class SearchTasksUseCase:
+    def __init__(self, task_repository: TaskRepository):
+        self.task_repository = task_repository
+
+    def execute(self, filters: SearchTaskFilters) -> List[Task]:
+        return self.task_repository.search_tasks(filters)
