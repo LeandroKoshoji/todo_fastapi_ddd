4d05a08d5307069e141b93412fa328cf2022f80d
Leandro Koshoji <leandro.koshoji@gmail.com>
2024-07-14 18:52:21 -0300

    test: test sqlachemy use repo

--- tests/integration/core/user/infra/repositories/test_sqlalchemy_user_repository.py
+++ tests/integration/core/user/infra/repositories/test_sqlalchemy_user_repository.py
@@ -1,7 +1,10 @@
 import pytest
 from sqlalchemy.orm import Session
 from app.core.user.domain.user import User, UserId
-from app.core.user.infra.repositories.sqlalchemy_user_repository import SqlAlchemyUserRepository
+from app.core.user.infra.repositories.sqlalchemy_user_repository import (
+    SqlAlchemyUserRepository
+)
+from tests.helpers import create_user
 
 
 @pytest.fixture
@@ -10,13 +13,58 @@ def user_repository(db_session: Session) -> SqlAlchemyUserRepository:
 
 
 @pytest.mark.integration
-def test_save_user(user_repository: SqlAlchemyUserRepository):
-    user = User(username="testuser", email="testuser@email.com",
-                hashed_password="hashed_password")
+def test_save_user(
+    user_repository: SqlAlchemyUserRepository,
+    db_session: Session
+):
+    user_id = UserId()
+    user = User(
+        id=user_id.id,
+        username="testuser",
+        email="testuser@email.com",
+        hashed_password="hashed_password"
+    )
     user_repository.save_user(user)
 
-    retrieved_user = user_repository.get_user_by_email(
-        email="testuser@email.com")
+    retrieved_user = user_repository.get_user_by_id(user_id.id)
     assert retrieved_user is not None
     assert retrieved_user.username == "testuser"
     assert retrieved_user.email == "testuser@email.com"
+
+
+@pytest.mark.integration
+def test_get_user_by_id(
+    user_repository: SqlAlchemyUserRepository,
+    db_session: Session
+):
+    user_id = create_user(db_session)
+    retrieved_user = user_repository.get_user_by_id(user_id.id)
+
+    assert retrieved_user is not None
+    assert retrieved_user.username == "testuser"
+    assert retrieved_user.email == "testuser@email.com"
+
+
+@pytest.mark.integration
+def test_get_user_by_email(
+    user_repository: SqlAlchemyUserRepository,
+    db_session: Session
+):
+    create_user(db_session)
+    retrieved_user = user_repository.get_user_by_email("testuser@email.com")
+
+    assert retrieved_user is not None
+    assert retrieved_user.username == "testuser"
+    assert retrieved_user.email == "testuser@email.com"
+
+
+@pytest.mark.integration
+def test_delete_user(
+    user_repository: SqlAlchemyUserRepository,
+    db_session: Session
+):
+    user_id = create_user(db_session).id
+    user_repository.delete_user(user_id)
+
+    retrieved_user = user_repository.get_user_by_id(user_id)
+    assert retrieved_user is None
